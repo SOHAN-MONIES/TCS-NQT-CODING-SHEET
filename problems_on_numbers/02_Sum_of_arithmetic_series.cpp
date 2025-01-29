9432cb28f4e23a4797d253c55ec4d1219d4c0b50
Sohan Monies <108331198+SOHAN-MONIES@users.noreply.github.com>
2025-01-29 11:30:51 +0530

    Add files via upload

---
+++ problems_on_numbers/02_Sum_of_arithmetic_series.cpp
@@ -0,0 +1,30 @@
+// https : // www.geeksforgeeks.org/problems/sum-of-ap-series4512/1
+
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+long sum_of_ap(long n, long a, long d)
+{
+    long sum = 0;
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return a;
+    else
+    {
+        sum = a;
+        for (int i = 2; i <= n; i++)
+        {
+            sum = sum + a + (i - 1) * d;
+        }
+
+        return sum;
+    }
+}
+
+int main()
+{
+    cout << sum_of_ap(5, 1, 3);
+    return 0;
+}

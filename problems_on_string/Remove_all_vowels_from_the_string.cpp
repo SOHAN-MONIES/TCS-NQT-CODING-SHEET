869eb7f458a3cbe84995022b26835e3094a66d23
SOHAN-MONIES <anon.marvin.sohan@gmail.com>
2025-02-04 16:47:04 +0530

    String Problems

--- problems_on_string/Remove_all_vowels_from_the_string.cpp
+++ problems_on_string/Remove_all_vowels_from_the_string.cpp
@@ -2,9 +2,24 @@
 #include <bits/stdc++.h>
 #include <iostream>
 using namespace std;
+string removeVowels(string &s)
+{
+    string res = "";
+    for (char c : s)
+    {
+        if (!(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+              c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U'))
+        {
+            res += c;
+        }
+    }
+    return res;
+}
 
 int main()
 {
-
+    string s1 = "SOHAN";
+    string s2 = removeVowels(s1);
+    cout << s2;
     return 0;
 }
